ae0a2933dc7b50f33cfe6d9fe7d62e8af26e2846
MarcosRanauro <marcosranauro@hotmail.com>
2022-09-28 18:58:20 -0300

    Fiz o exercicio 2

---
+++ fundamentos/secao-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/Exercicio 2/script.js
@@ -0,0 +1,6 @@
+let paragrafo = document.getElementsByClassName("paragrafos");
+    paragrafo[0].style.backgroundColor = "blue";
+
+document.getElementsByTagName("h4")[0].style.color = "orange";
+
+    
