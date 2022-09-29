347c39b4c9c107d645251a3573d41c34bb97903b
MarcosRanauro <marcosranauro@hotmail.com>
2022-09-29 19:27:41 -0300

    Fiz novos ajustes

--- fundamentos/secao-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/Exercicio 1/script.js
+++ fundamentos/secao-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/Exercicio 1/script.js
@@ -1,16 +1,18 @@
-document.querySelectorAll("#pai")[0].style.color = "red"
-document.querySelectorAll("#primeiroFilhoDoFilho")[0].innerHTML = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+document.getElementById("elementoOndeVoceEsta").parentNode.style.color = "red";
+
+// Acesse o primeiroFilhoDoFilho e adicione um texto a ele.
+document.getElementById("elementoOndeVoceEsta").firstElementChild.innerHTML = "Lorem Ipsum is simply dummy text ofdsfsdfsdf thr";
 
 // Acesse o primeiroFilho a partir de pai.
-document.querySelector("#pai").firstChild;
+document.querySelector("#pai").firstElementChild.innerHTML = "lalalalalala";
 
-// Agora acesse o primeiroFilho a partir de elementoOndeVoceEsta.
-document.querySelector("#elementoOndeVoceEsta").previousElementSibling;
+// // Agora acesse o primeiroFilho a partir de elementoOndeVoceEsta.
+document.querySelector("#elementoOndeVoceEsta").previousElementSibling.style.backgroundColor = "blue";
 
-// Agora acesse o texto Atenção! a partir de elementoOndeVoceEsta.
-document.querySelector("#elementoOndeVoceEsta").nextSibling
+// // Agora acesse o texto Atenção! a partir de elementoOndeVoceEsta.
+document.getElementById("elementoOndeVoceEsta").nextSibling;
 
-// Agora acesse o terceiroFilho a partir de elementoOndeVoceEsta.
-document.querySelector("#elementoOndeVoceEsta").nextElementSibling;
+// // Agora acesse o terceiroFilho a partir de elementoOndeVoceEsta.
+document.querySelector("#elementoOndeVoceEsta").nextElementSibling.innerHTML = "lelelele";
 
-// Agora acesse o terceiroFilho a partir de pai.
+// // Agora acesse o terceiroFilho a partir de pai.
