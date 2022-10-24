f39ed9c8b48a41b0dad1f1ac7268c5a669cf0820
MarcosRanauro <marcosranauro@hotmail.com>
2022-10-24 18:26:46 -0300

    Inclui a resolução do course

--- fundamentos/secao-8-higher-order-functions-do-javascript-ES6/dia-1-javascript-ES6-introducao-a-higher-order-functions/script.js
+++ fundamentos/secao-8-higher-order-functions-do-javascript-ES6/dia-1-javascript-ES6-introducao-a-higher-order-functions/script.js
@@ -1,3 +1,4 @@
+//! Minha resolução
 const frase1 = () => 'Acordando!!';
 const frase2 = () => 'Bora tomar café!!';
 const frase3 = () => 'Partiu dormir!!';
@@ -7,3 +8,15 @@ const doingThings = (func) => func ();
 console.log(doingThings(frase1));
 console.log(doingThings(frase2));
 console.log(doingThings(frase3));
+
+//! Resolução do course
+const wakeUp = () => 'Acordando!!';
+const breakfast = () => 'Bora tomar café!!';
+const sleep = () => 'Partiu dormir!!';
+const doingThings = (callback) => {
+  const result = callback();
+  console.log(result);
+};
+doingThings(wakeUp); // Acordando!!
+doingThings(breakfast); // Bota tomar café!!
+doingThings(sleep); // Partiu dormir!!
