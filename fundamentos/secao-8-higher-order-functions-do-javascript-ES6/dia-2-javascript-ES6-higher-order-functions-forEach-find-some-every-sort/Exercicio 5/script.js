dba65d0c77ba29cb42d3b0db5269283d0ad81d96
MarcosRanauro <marcosranauro@hotmail.com>
2022-10-25 17:13:30 -0300

    Exercicios do dia das aulas do course

---
+++ fundamentos/secao-8-higher-order-functions-do-javascript-ES6/dia-2-javascript-ES6-higher-order-functions-forEach-find-some-every-sort/Exercicio 5/script.js
@@ -0,0 +1,12 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const multipliesFor2 = (element) => {
+  console.log(`${element} * 3 = ${element * 3}`);
+};
+
+numbers.forEach(multipliesFor2);
+
+const multiply = (numbers, multiply) => {
+    let newNumbers = [];
+    
+}
