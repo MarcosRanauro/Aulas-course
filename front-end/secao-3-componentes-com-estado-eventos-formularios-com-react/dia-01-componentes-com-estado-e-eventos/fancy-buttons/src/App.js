395d1ad9f38f0b33ef36a3fd7866b42fefc26467
MarcosRanauro <marcosranauro@hotmail.com>
2022-11-30 14:50:06 -0300

    Incluindo logica de contador com cores

--- front-end/secao-3-componentes-com-estado-eventos-formularios-com-react/dia-01-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
+++ front-end/secao-3-componentes-com-estado-eventos-formularios-com-react/dia-01-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
@@ -25,31 +25,44 @@ class App extends React.Component {
     this.state = {
       numeroClicksOne: 0,
       numeroClicksTwo: 0,
-      numeroClicksThree: 0
+      numeroClicksThree: 0,
     }
   }
 
   clickOne () {
+    const { numeroClicksOne } = this.state;
     console.log('Click 1');
     this.setState((estadoAnterior) => ({
       numeroClicksOne: estadoAnterior.numeroClicksOne + 1
-    }))
+    }), () => {
+      console.log(`Botão 1 ${this.getButtonColor(numeroClicksOne)}`);
+    });
   }
   
   clickTwo () {
+    const { numeroClicksTwo } = this.state;
     console.log('Click 2');
     this.setState((estadoAnterior2) => ({
       numeroClicksTwo: estadoAnterior2.numeroClicksTwo + 1
-    }))
+    }), () => {
+      console.log(`Botão 2 ${this.getButtonColor(numeroClicksTwo)}`);
+    })
   }
   
   clickThree () {
+    const { numeroClicksThree } = this.state;
     console.log('Click 3');
     this.setState((estadoAnterior3) => ({
       numeroClicksThree: estadoAnterior3.numeroClicksThree + 1
-    }))
+    }), () => {
+      console.log(this.getButtonColor(numeroClicksThree));
+    })
   }
   //! Função com constructor  🔼  🔼 
+
+  getButtonColor (num) {
+    return num % 2 === 1 ? 'green' : 'white'
+  }
 
   //! Usando a função com arrow functions sem o constructor declarado 🔽 🔽 
   // clickOne = () => {
@@ -66,6 +79,8 @@ class App extends React.Component {
 
 
   render() {
+    const { clickOne, clickTwo, clickThree } = this.state;
+    const { numeroClicksOne, numeroClicksTwo, numeroClicksThree } = this.state;
     return (
       //! Com função fora da class
       // <div>
@@ -76,9 +91,29 @@ class App extends React.Component {
 
       //! Com função dentro da class usando This.
       <div>
-        <button onClick={this.clickOne}>{this.state.numeroClicksOne}</button>
-        <button onClick={this.clickTwo}>{this.state.numeroClicksTwo}</button>
-        <button onClick={this.clickThree}>{this.state.numeroClicksThree}</button>
+        <button
+          type="button"
+          onClick={this.clickOne}
+          style={ { backgroundColor: this.getButtonColor(clickOne) } }
+        >
+          {`Botão 1 ${numeroClicksOne}`};
+        </button>
+
+        <button
+          type="button"
+          onClick={this.clickTwo}
+          style={ { backgroundColor: this.getButtonColor(clickTwo) } }
+        >
+          {`Botão 1 ${numeroClicksTwo}`};
+        </button>
+
+        <button
+          type="button"
+          onClick={this.clickThree}
+          style={ { backgroundColor: this.getButtonColor(clickThree) } }
+        >
+          {`Botão 1 ${numeroClicksThree}`};
+        </button>
       </div>
     );
   }
