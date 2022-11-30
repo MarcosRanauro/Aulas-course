0f4e5dd8c73944baeb95cd0179b40c576de9da19
MarcosRanauro <marcosranauro@hotmail.com>
2022-11-29 22:44:56 -0300

    Refatorando usando this com função dentro da class

--- front-end/secao-3-componentes-com-estado-eventos-formularios-com-react/dia-01-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
+++ front-end/secao-3-componentes-com-estado-eventos-formularios-com-react/dia-01-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
@@ -1,26 +1,48 @@
 import './App.css';
 import React from 'react';
 
-function clickOne() {
-  console.log('Click 1');
-}
+// function clickOne() {
+//   console.log('Click 1');
+// }
 
-function clickTwo() {
-  console.log('Click 2');
-}
+// function clickTwo() {
+//   console.log('Click 2');
+// }
 
-function clickThree() {
-  console.log('Click 3');
-}
+// function clickThree() {
+//   console.log('Click 3');
+// }
 
 
 class App extends React.Component {
+
+  clickOne = () => {
+    console.log('Click 1');
+  }
+  
+  clickTwo = () => {
+    console.log('Click 2');
+  }
+  
+  clickThree = () => {
+    console.log('Click 3');
+  }
+
+
   render() {
     return (
+      //! Com função fora da class
+      // <div>
+      //   <button onClick={clickOne}>Botão 1</button>
+      //   <button onClick={clickTwo}>Botão 2</button>
+      //   <button onClick={clickThree}>Botão 3</button>
+      // </div>,
+
+      //! Com função dentro da class usando This.
       <div>
-      <button onClick={clickOne}>Botão 1</button>
-      <button onClick={clickTwo}>Botão 2</button>
-      <button onClick={clickThree}>Botão 3</button>
+        <button onClick={this.clickOne}>Botão 1</button>
+        <button onClick={this.clickTwo}>Botão 2</button>
+        <button onClick={this.clickThree}>Botão 3</button>
       </div>
     );
   }
